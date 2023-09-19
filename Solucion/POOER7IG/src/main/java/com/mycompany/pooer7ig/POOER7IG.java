c450f827329001549abfd742a412b6f789f05b5f
Julian Ruiz Ocampo <124713530+JuliRuizOcampo@users.noreply.github.com>
2023-09-19 00:25:22 -0500

    E7

---
+++ Solucion/POOER7IG/src/main/java/com/mycompany/pooer7ig/POOER7IG.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooer7ig;
+
+/**
+ *
+ * @author julir
+ */
+public class POOER7IG {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
