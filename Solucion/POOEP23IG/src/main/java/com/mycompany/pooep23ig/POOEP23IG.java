4e8a3eda66c2a54e67fefb69e5ba86e3d4c626c7
Julian Ruiz Ocampo <124713530+JuliRuizOcampo@users.noreply.github.com>
2023-09-19 00:32:47 -0500

    E23

---
+++ Solucion/POOEP23IG/src/main/java/com/mycompany/pooep23ig/POOEP23IG.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooep23ig;
+
+/**
+ *
+ * @author julir
+ */
+public class POOEP23IG {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
