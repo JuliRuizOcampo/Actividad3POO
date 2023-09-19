7a39a47ac2d6da67c0ed63ece2ba5e1285666651
Julian Ruiz Ocampo <124713530+JuliRuizOcampo@users.noreply.github.com>
2023-09-19 00:37:26 -0500

    PF

---
+++ Solucion/PruebaFigurasIG/src/main/java/com/mycompany/pruebafigurasig/PruebaFigurasIG.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebafigurasig;
+
+/**
+ *
+ * @author julir
+ */
+public class PruebaFigurasIG {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+        
+    }
+}
+
